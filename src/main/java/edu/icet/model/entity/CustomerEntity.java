d911b93b23c41aab86317bed1883af840b88abba
Yasith Samarakoon <samarakoonyasi21@gmail.com>
2026-01-02 13:06:27 +0530

    set path to get Admin details with AdminSignInService

--- src/main/java/edu/icet/model/entity/CustomerEntity.java
+++ src/main/java/edu/icet/model/entity/CustomerEntity.java
@@ -1,6 +1,14 @@
 package edu.icet.model.entity;
 
-public class CustomerEntity {
+import java.time.LocalDate;
 
+public class CustomerEntity {
+    private String id;
+    private String name;
+    private LocalDate dateOfBirth;
+    private String email;
+    private String password;
+    private String address;
+    private int postalCode;
 
 }
--- src/main/java/edu/icet/service/AdminSignInService.java
+++ src/main/java/edu/icet/service/AdminSignInService.java
@@ -1,7 +1,38 @@
 package edu.icet.service;
 
+import edu.icet.model.dto.AdminDto;
+import edu.icet.model.entity.AdminEntity;
+import edu.icet.repository.AdminSignInRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class AdminSignInService {
 
+    AdminSignInRepository adminSignInRepository;
 
+    public List<AdminDto> getAllAdmin(){
+
+        List<AdminEntity> adminEntity = adminSignInRepository.findAll();
+        List<AdminDto> adminDtos = new ArrayList<>();
+
+        for(AdminEntity entityAdmin : adminEntity){
+            adminDtos.add(new AdminDto(
+
+                    entityAdmin.getId(),
+                    entityAdmin.getName(),
+                    entityAdmin.getDateOfBirth(),
+                    entityAdmin.getEmail(),
+                    entityAdmin.getPassword(),
+                    entityAdmin.getAddress(),
+                    entityAdmin.getPostalCode()
+
+            ));
+
+        }
+        return adminDtos;
+    }
 
 }
