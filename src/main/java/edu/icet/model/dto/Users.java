ac14025b2e55dde786093ea70e35e612aa135bd5
Yasith Samarakoon <samarakoonyasi21@gmail.com>
2026-02-02 15:56:54 +0530

    employee signin

--- src/main/java/edu/icet/model/dto/Users.java
+++ src/main/java/edu/icet/model/dto/Users.java
@@ -1,8 +1,22 @@
 package edu.icet.model.dto;
 
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+
 public class Users {
 
+    @Email(message = "Enter your correct email")
     private String email;
 
+
+    @NotBlank(message = "enter is required")
     private String password;
 }
--- src/main/java/edu/icet/model/entity/UsersEntity.java
+++ src/main/java/edu/icet/model/entity/UsersEntity.java
@@ -1,6 +1,21 @@
 package edu.icet.model.entity;
 
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString(exclude = "password")
+
 public class UsersEntity {
+
+    @
+    private String email;
+
+
+    private String password;
 
 
 }
